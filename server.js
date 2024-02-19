125f464aedf76f8348aa950d060b6247e8227dbc
Aaron Yup <aaron.yup@toolup.com>
2024-02-19 14:42:30 -0800

    limit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-app.use(express.json());
+app.use(express.json({ limit: '80mb' }))
 var post = null;
 const {google} = require('googleapis');
 const drive = google.drive('v3');
